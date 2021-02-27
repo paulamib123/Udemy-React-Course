fd6fbca76714a72e3da1283bb7407e69fe25c97e
Paulami Bhattacharya <paulamixbhattacharya@gmail.com>
2021-02-27 22:27:42 +0530

    added github-finder

---
+++ github-finder/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;

dd318ef5b8af5f81d5c32456c9f1bf6e1a0f7995
Paulami Bhattacharya <paulamixbhattacharya@gmail.com>
2021-04-13 18:31:06 +0530

    added it-logger project

---
+++ it-logger/src/App.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+function App() {
+  useEffect(() => {
+    M.AutoInit();
+  })
+
+  return (
+    <div className="App">
+      My App
+    </div>
+  );
+}
+
+export default App;
